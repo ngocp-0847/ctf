417af771c4453a27ffef47dffc552e25526b22d9
phan.ngoc <phan.ngoc@sun-asterisk.com>
2020-10-05 07:29:31 +0700

    [Add] Custom page for CTF test

---
+++ wp-content/themes/twentytwenty/template-parts/content-test.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Displays the content when the cover template is used.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+?>
+
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	<div class="post-inner" id="post-inner">
+
+		<div class="entry-content">
+
+		<?php
+		the_content();
+		?>
+
+		</div><!-- .entry-content -->
+		<?php
+		wp_link_pages(
+			array(
+				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Page', 'twentytwenty' ) . '"><span class="label">' . __( 'Pages:', 'twentytwenty' ) . '</span>',
+				'after'       => '</nav>',
+				'link_before' => '<span class="page-number">',
+				'link_after'  => '</span>',
+			)
+		);
+
+		edit_post_link();
+		// Single bottom post meta.
+		twentytwenty_the_post_meta( get_the_ID(), 'single-bottom' );
+
+		if ( post_type_supports( get_post_type( get_the_ID() ), 'author' ) && is_single() ) {
+
+			get_template_part( 'template-parts/entry-author-bio' );
+
+		}
+		?>
+
+	</div><!-- .post-inner -->
+
+	<?php
+
+	if ( is_single() ) {
+
+		get_template_part( 'template-parts/navigation' );
+	}
+
+	/**
+	 *  Output comments wrapper if it's a post, or if comments are open,
+	 * or if there's a comment number – and check for password.
+	 * */
+	if ( ( is_single() || is_page() ) && ( comments_open() || get_comments_number() ) && ! post_password_required() ) {
+		?>
+
+		<div class="comments-wrapper section-inner">
+
+			<?php comments_template(); ?>
+
+		</div><!-- .comments-wrapper -->
+
+		<?php
+	}
+	?>
+    <style type="text/css">
+        h1.entry-title {
+            text-align: center;
+            margin-top: 32px;
+        }
+    </style>
+</article><!-- .post -->
